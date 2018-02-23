4f638907ba60df6ff92c46d7775eea67cba6b9c1
hfzhae <18657885@qq.com>
2018-02-23 13:23:53 +0800

    单据测试

--- client/js/main.js
+++ client/js/main.js
@@ -65,7 +65,7 @@ require(['config'], function () {
 		title:'系统'
 	});
 	$('.easyui-accordion').accordion('select', 0);
-	$('#hometitle').append('<img src="client/images/EBLOGO.png" class="homelogo"><div class="hometitle">e商3000</div><div class="homeversion">v3.0</div><div class="hometitlecopyright">Design by zydsoft™</div>');
+	$('#hometitle').append('<img src="client/images/EBLOGO.png" class="homelogo"><div class="hometitle">e商x</div><div class="homeversion">v1.0</div><div class="hometitlecopyright">Design by zydsoft™</div>');
 	//$('body').css({padding:5});
 	//$('.easyui-layout').css({width:($(window).width()-10),height:($(window).height()-10)});
 	//require(['easyui'], function(){
