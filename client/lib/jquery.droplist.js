262d181c5712422c7107f96b82fb38994d8aeac5
hfzhae <18657885@qq.com>
2018-02-28 21:08:00 +0800

    C:/Program Files/Git/

--- client/lib/jquery.droplist.js
+++ client/lib/jquery.droplist.js
@@ -9,6 +9,8 @@ dev by zz on 2018/2/16
 options.columns.search（bool）:true的字段支持搜索；
 options.oRows:存储原始数据
 options.asynurl:异步读取的url地址
+options.asyn:是否从服务器端读取数据，0为读取，默认0，读取后置1，按esc键置0
+
 *****************************************************************/
 
 $.extend($.fn.combogrid.defaults, {
@@ -68,8 +70,10 @@ $.extend($.fn.combogrid.defaults, {
 				for (var i in rows) {
 					serachon = 0
 					for(var j in searchField){
-						if (rows[i][searchField[j]].toLowerCase().indexOf(t.toLowerCase()) != -1) {
-							serachon = 1;
+						if(rows[i][searchField[j]] != undefined){
+							if (rows[i][searchField[j]].toLowerCase().indexOf(t.toLowerCase()) != -1) {
+								serachon = 1;
+							}
 						}
 					}
 					if (serachon == 1) {
@@ -100,16 +104,23 @@ $.extend($.fn.combogrid.defaults, {
 					g.datagrid('unselectRow', index);//键盘响应，取消选中的行
 				}
 			} catch(err) {}
+		},
+		esc: function(){
+			var co = $(this)
+			g = co.combogrid('grid'),
+			ops = co.combogrid('options')
+			ops.asyn = 0;
 		}
 	},
 	onShowPanel: function(){
 		var co = $(this)
 			g = co.combogrid('grid'),
+			ops = co.combogrid('options'),
 			pageSize = 50;
-		if(g.datagrid('getRows').length == 0){
+			
+		if(g.datagrid('getRows').length == 0 || !ops.asyn){
 			var t = $.trim(co.combogrid('getText')),
 				mask = $('<div class="datagrid-mask" style="display:block"></div><div class="datagrid-mask-msg" style="display: block; left: 50%; height: 16px; margin-left: -98px; line-height: 16px;">Processing, please wait ...</div>'),
-				ops = co.combogrid('options'),
 				url = ops.asynurl;
 			g.parent().append(mask);
 			g.datagrid({
@@ -117,6 +128,7 @@ $.extend($.fn.combogrid.defaults, {
 				pageSize:pageSize
 			});
 			ops.oRows = [];
+			ops.asyn = 1;
 			$.getJSON(url, { v: (new Date()).getTime() }, function(data){				
 				for(var i in data['rows']){
 					ops.oRows.push(data['rows'][i]);
@@ -132,49 +144,50 @@ $.extend($.fn.combogrid.defaults, {
 	},
 	inputEvents: {//键盘事件重造
 		keydown: function(e){
-		var _a2e = e.data.target;
-		var t = $(_a2e);
-		var _a2f = t.data("combo");
-		var opts = t.combo("options");
-		_a2f.panel.panel("options").comboTarget = _a2e;
-		switch (e.keyCode) {
-			case 38:
-				opts.keyHandler.up.call(_a2e, e);
-				break;
-			case 40:
-				opts.keyHandler.down.call(_a2e, e);
-				break;
-			case 37:
-				opts.keyHandler.left.call(_a2e, e);
-				break;
-			case 39:
-				opts.keyHandler.right.call(_a2e, e);
-				break;
-			case 13:
-				e.preventDefault();
-				opts.keyHandler.enter.call(_a2e, e);
-				return false;
-			case 9:
-			case 27:
-				var _a39 = $.data(_a2e, "combo").panel;
-				_a39.panel("close");
-				break;
-			default:
-				if (opts.editable) {
-					if (_a2f.timer) {
-						clearTimeout(_a2f.timer);
-					}
-					_a2f.timer = setTimeout(function() {
-						var q = t.combo("getText");
-						if (_a2f.previousText != q) {
-							_a2f.previousText = q;
-							t.combo("showPanel");
-							opts.keyHandler.query.call(_a2e, q, e);
-							t.combo("validate");
+			var _a2e = e.data.target;
+			var t = $(_a2e);
+			var _a2f = t.data("combo");
+			var opts = t.combo("options");
+			_a2f.panel.panel("options").comboTarget = _a2e;
+			switch (e.keyCode) {
+				case 38:
+					opts.keyHandler.up.call(_a2e, e);
+					break;
+				case 40:
+					opts.keyHandler.down.call(_a2e, e);
+					break;
+				case 37:
+					opts.keyHandler.left.call(_a2e, e);
+					break;
+				case 39:
+					opts.keyHandler.right.call(_a2e, e);
+					break;
+				case 13:
+					e.preventDefault();
+					opts.keyHandler.enter.call(_a2e, e);
+					return false;
+				case 9:
+				case 27:
+					var _a39 = $.data(_a2e, "combo").panel;
+					_a39.panel("close");
+					opts.keyHandler.esc.call(_a2e, e);//按esc键重置查询标志，重置后，需要读取服务器端数据
+					break;
+				default:
+					if (opts.editable) {
+						if (_a2f.timer) {
+							clearTimeout(_a2f.timer);
 						}
-					},
-					opts.delay);
-				}
+						_a2f.timer = setTimeout(function() {
+							var q = t.combo("getText");
+							if (_a2f.previousText != q) {
+								_a2f.previousText = q;
+								t.combo("showPanel");
+								opts.keyHandler.query.call(_a2e, q, e);
+								t.combo("validate");
+							}
+						},
+						opts.delay);
+					}
 			}
 		}
 	}
