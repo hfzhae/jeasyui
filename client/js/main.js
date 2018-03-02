abc1b028ff7f135a5ada7acc757a6b4e2052ef50
hfzhae <18657885@qq.com>
2018-03-02 22:16:53 +0800

    支持切换主题

--- client/js/main.js
+++ client/js/main.js
@@ -2,9 +2,8 @@
 Copyright (c) 2018 by ZYDSOFT Company. ALL RIGHTS RESERVED.
 dev by zz on 2018/2/16
 *****************************************************************/
-
 easyloader.base = 'client/lib/easyui/';
-easyloader.theme = 'material';
+easyloader.theme = getThemes();
 easyloader.locale = "zh_CN";
 
 easyloader.load([
@@ -15,7 +14,8 @@ easyloader.load([
 	'tabs', 
 	'tree', 
 	'messager', 
-	'datebox'
+	'datebox',
+	'menubutton'
 ], function(){
 	$('body').append('<div id="bodylaout">');
 	var bl = $('#bodylaout')
@@ -24,12 +24,22 @@ easyloader.load([
 		}).layout('add',{
 			region: 'west',
 			width: 200,
-			//title: '功能菜单',
+			title: '',
 			href: 'west.html',
-			split: false
+			hideExpandTool:false,
+			hideCollapsedContent:false,
+			split: false,
+			collapsedContent: function(title){
+				var region = $(this).panel('options').region;
+				if(region =='north'|| region =='south'){
+					//返回标题;
+				} else {
+					return '<div class="panel-title layout-expand-title layout-expand-title-down">功能菜单</ div>';
+				}
+			}
 		}).layout('add',{
 			region: 'north',
-			height: 23,
+			height: 25,
 			href:'north.html',
 			border:false,
 			split: false
@@ -48,3 +58,11 @@ easyloader.load([
 			bl.layout('resize')//.layout('collapse', 'east');
 		}, 300);
 });
+
+function getThemes(){
+	var Storage = window.localStorage;
+	if(Storage.getItem("themes") === null){
+		Storage.setItem('themes', 'default');
+	}
+	return Storage.getItem("themes")
+}
