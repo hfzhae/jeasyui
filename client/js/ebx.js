609c6a60577c3a4f857efd87832f07f4fd479373
hfzhae <18657885@qq.com>
2018-05-15 17:09:14 +0800

    C:/Program Files/Git/

--- client/js/ebx.js
+++ client/js/ebx.js
@@ -5,7 +5,7 @@ dev by zz on 2018/2/16
 var ebx = {
 	multitabs:0,//同一菜单链接多tabs打开支持，1为支持
 	decimal:2,//小数位数，默认2
-	pagesize: 50,//datagrid分页行数
+	pagesize: 128,//datagrid分页行数
 	init: function(){
 		easyloader.base = 'client/lib/easyui/';
 		easyloader.theme = this.getThemes();
@@ -20,7 +20,8 @@ var ebx = {
 			'tree', 
 			'messager', 
 			'datebox',
-			'menubutton'
+			'menubutton',
+			'clipboard'
 		], function(){
 			ebx.bodylayout = $('<div>').appendTo($('body'));//定义全局layout
 			var bl = ebx.bodylayout
@@ -74,6 +75,9 @@ var ebx = {
 				var e = window.event||e;  
 				e.returnValue=("有数据没有保存，确定离开当前页面吗？");
 			}
+		}
+		String.prototype.replaceAll = function(s1,s2){ 
+			return(this.replace(new RegExp(s1,"gm"),s2)); 
 		}
  	},
 	getThemes: function(){//获取主题函数
@@ -179,6 +183,83 @@ var ebx = {
 		}else{
 			return n;
 		}
+	},
+	clipboardData: function (columns, data){//复制到剪贴板方法，参数：columns：表头对象，data：数据内容，包含total和rows 2018-5-15 zz
+		if(typeof(columns) != 'object')return;
+		if(typeof(data) != 'object')return;
+		if(ebx.validInt(columns[0].length) <= 0)return;
+		if(ebx.validInt(data.total) <= 0)return;
+		
+		var s = "",
+			centererpanelwindow = $('<div style="text-align:center;padding:5px;"><p>成功读取：'+ data.total +' 条数据。</p></div>').appendTo($('body')),
+			copybtn = $('<div name="copybtn">').appendTo(centererpanelwindow);
+		
+		for(var i in columns[0]){//表头文字
+			s += ebx.unescapeEx(columns[0][i].title.toString().replaceAll(' ','_').replaceAll('	', '_').replaceAll('　', '_')) + '	';
+		}
+		s = s.substr(0, s.length - 1);
+		s += '\n';
+		for(var i in data.rows){//内容文字
+			for(var j in columns[0]){//按表头顺序加载
+				for(var k in data.rows[i]){
+					if(columns[0][j].field == k){
+						s += ebx.unescapeEx(data.rows[i][k].toString().replaceAll(' ','_').replaceAll('	', '_').replaceAll('　', '_')) + '	';
+					}
+				}
+			}
+			s = s.substr(0, s.length - 1);
+			s += '\n';
+		}
+		
+		copybtn.linkbutton({
+			text:'复制到剪贴板',
+			iconCls: 'icon-Copy-large'
+		})
+		.addClass('l-btn-large')
+		//.addClass('l-btn-plain')
+		.find('.l-btn-left')
+		.removeClass('l-btn-icon-left')
+		.addClass('l-btn-icon-top');
+		
+		centererpanelwindow.window({
+			width:250,
+			height:160,
+			iconCls:'icon-ImportExcel',
+			modal:true,
+			collapsible:false,
+			minimizable:false,
+			maximizable:false,
+			closable:true,
+			title:'导出',
+			draggable:false,
+			resizable:false,
+			shadow:false
+		});
+								
+		var clipboard = new Clipboard('div[name=copybtn]', {  
+			text: function() {
+				return s;
+			}
+		});
+		
+		clipboard.on('success', function(e) {  
+			centererpanelwindow.window('close');
+			centererpanelwindow.remove();
+			clipboard.destroy();
+			$.messager.show({
+				title: '复制成功',
+				msg: data.total + ' 条数据已经复制到剪贴板，请打开Excel，点击“粘贴”按钮。',
+				timeout: 5000,
+				height:120,
+				showType: 'slide'
+			});
+		});  
+	  
+		clipboard.on('error', function(e) {  
+			centererpanelwindow.window('close');
+			centererpanelwindow.remove();
+			clipboard.destroy();
+		}); 
 	}
 };
 
--- server/bi/list/default.asp
+++ server/bi/list/default.asp
@@ -2,15 +2,21 @@
 <%
 (function(){
 	if(!ebx.stdin['template']) return([]);
-	var page = {//iStart锛氳捣濮嬭鏁帮紝iLength锛氭瘡椤佃鏁帮紝iTotalLength锛氭�昏鏁帮紙鍥炶皟鐢級
+
+	var page = {//iStart：起始行数，iLength：每页行数，iTotalLength：总行数（回调用）
 			iStart: ebx.validInt(ebx.stdin['page']>1?((ebx.stdin['page'] - 1) * ebx.stdin['rows']) + 1: 1), 
 			iLength: ebx.validInt(ebx.stdin['rows']), 
 			iTotalLength: 0
 		};
-	
-	var rs = ebx.dbx.openpage(ebx.getTemplateSQL(ebx.validInt(ebx.stdin['template'])), page);//閫氳繃鏌ヨ妯℃澘鑾峰彇sql璇彞
+
 	var data = new Array();
-	data["total"] = page.iTotalLength;
+	if(ebx.validInt(ebx.stdin['exportdata'], 0) == 0){
+		var rs = ebx.dbx.openpage(ebx.getTemplateSQL(ebx.validInt(ebx.stdin['template'])), page);//分页查询语句
+		data["total"] = page.iTotalLength;
+	}else{
+		var rs = ebx.dbx.open(ebx.getTemplateSQL(ebx.validInt(ebx.stdin['template'])), 1, 1);//全部导出用语句，参数exportdata=1
+		data["total"] =rs.RecordCount;
+	}
 	data["rows"] = rs;
 	data["footer"] = [{}];
 	ebx.stdout = data;
